ede9d79bb93668367f1ada66c9aa30dd44c34106
David Herszenhaut <david.herszenhaut@gmail.com>
2020-05-03 15:45:06 -0400

    Copied over contents from davidherszenhaut/portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,50 @@
+// @format
 import React from "react"
+import Header from "../components/Header"
+import Hero from "../components/Hero"
+import Footer from "../components/Footer"
+import Projects from "../components/Projects"
+import Resume from "../components/Resume"
 
-export default () => <div>Hello world!</div>
+import about from "../../content/data/about.json"
+import projects from "../../content/data/projects.json"
+import resume from "../../content/data/resume.json"
+
+import "../styles/App.css"
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleProjectsChange = this.handleProjectsChange.bind(this)
+    this.handleResumeChange = this.handleResumeChange.bind(this)
+    this.state = {
+      tab: "projects",
+    }
+  }
+
+  handleProjectsChange() {
+    this.setState({ tab: "projects" })
+  }
+
+  handleResumeChange() {
+    this.setState({ tab: "resume" })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header
+          onProjectsChange={this.handleProjectsChange}
+          onResumeChange={this.handleResumeChange}
+          activeTab={this.state.tab}
+        />
+        <div className="contentContainer">
+          <Hero data={about} />
+          {this.state.tab === "projects" ? <Projects data={projects} /> : null}
+          {this.state.tab === "resume" ? <Resume data={resume} /> : null}
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+}

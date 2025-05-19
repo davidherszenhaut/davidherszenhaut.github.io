dfc3f856c7702acb7e762a1c3764c02efb6907a6
David Herszenhaut <davidherszenhaut.social@gmail.com>
2025-05-18 21:28:13 -0400

    removed old custom-ssg-version pages and moved favicon files

--- scripts/createFooter.js
+++
@@ -1,9 +0,0 @@
-export function createFooter() {
-  const footer = document.createElement("footer");
-  const footerParagraph = document.createElement("p");
-  const text = "David Herszenhaut \u00A9 " + new Date().getFullYear();
-  const footerText = document.createTextNode(text);
-  footerParagraph.appendChild(footerText);
-  footer.appendChild(footerParagraph);
-  document.body.appendChild(footer);
-}
